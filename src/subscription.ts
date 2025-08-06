d6d5f2366862621d994d6cf5f2aeeae025986281
Miztan <miketam317@hotmail.com>
2025-08-06 23:16:31 +0800

    bug修复

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,12 +4,12 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
-  version: 0,
-  author: 'author',
+  id: 99810,
+  name: 'Miz的GKD订阅',
+  version: 1,
+  author: 'Miz',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/Miztan/GKD-Subscription',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
