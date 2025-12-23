392dff56f76ae293f0be3ac78adcb6083533436a
Miztan <miketam317@hotmail.com>
2025-12-23 17:51:26 +0800

    删除荣耀自动刷脸功能

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -1,17 +1,3 @@
 import { defineGkdGlobalGroups } from '@gkd-kit/define';
 
-export default defineGkdGlobalGroups([
-  {
-    key: 0,
-    name: '功能类-自动点击扫脸确认',
-    desc: '荣耀系统自动点击刷脸通过后的确认按钮',
-    enable: true,
-    order: 0,
-    rules: [
-      {
-        fastQuery: true,
-        matches: 'Button[id="androidhnext:id/facebuttonmakesure"][text="确认"]',
-      },
-    ],
-  },
-]);
+export default defineGkdGlobalGroups([]);
